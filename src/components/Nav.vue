0932691ae97f1c0265ce602b03845b9225c47f07
yushirui <2514904609@qq.com>
2020-08-25 18:37:11 +0800

    feat:添加全局样式和变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money" />记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels" />标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics" />统计
     </router-link>
   </nav>
@@ -17,6 +17,7 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 255, 0.25);
@@ -34,6 +35,9 @@ nav {
       width: 32px;
       height: 32px;
     }
+  }
+  > .item.selected {
+    color: $color-highlight;
   }
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,54 @@
 <template>
   <div>
-    <Layout>money</Layout>
+    <Layout>
+      <div class="tags">
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+      </div>
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text" />
+        </label>
+      </div>
+      <div>
+        <ul class="tags">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+      <div class="numberPad">
+        <div class="output">100</div>
+        <div class="buttons">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>0</button>
+          <button style="grid-area: add" @click="append('+')">+</button>
+          <button style="grid-area: subtract" @click="append('-')">-</button>
+          <button style="grid-area: multipy" @click="append('×')">×</button>
+          <button style="grid-area: divide" @click="append('÷')">÷</button>
+          <button>.</button>
+          <button>%</button>
+          <button>删除</button>
+          <button>清空</button>
+          <button>OK</button>
+        </div>
+      </div>
+    </Layout>
   </div>
 </template>
 
