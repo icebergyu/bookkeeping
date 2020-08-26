5a7d37211677abb3ddb707384e4cc271a69fc728
yushirui <2514904609@qq.com>
2020-08-26 18:18:28 +0800

    feat:重新封装recordList.ts

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,15 +1,17 @@
 const localStrageKeyName = 'recordList';
 const recordListModel = {
+    data: [] as RecordItem[],
     clone(data: RecordItem | RecordItem[]) {
         return JSON.parse(JSON.stringify(data));
     },
     fetch() {
-        return JSON.parse(
+        this.data = JSON.parse(
             window.localStorage.getItem(localStrageKeyName) || '[]'
         ) as RecordItem[];
+        return this.data;
     },
-    save(data: RecordItem[]) {
-        window.localStorage.setItem(localStrageKeyName, JSON.stringify(data));
+    save() {
+        window.localStorage.setItem(localStrageKeyName, JSON.stringify(this.data));
     }
 };
 export default recordListModel 
