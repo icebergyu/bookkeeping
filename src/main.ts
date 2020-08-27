a1df9239882413153b2f8deda1ab804a7c9fb7da
yushirui <2514904609@qq.com>
2020-08-27 22:12:19 +0800

    feat:部署到码云

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ window.onload = function () {
 if (document.documentElement.clientWidth > 500) {
   window.alert("请使用手机打开页面")
   const img = document.createElement('img')
-  img.src = '/bookkeeping-website/qrcode.png'
+  img.src = '/bookkeeping-website/qrcode_gitee.png'
   img.style.position = 'fixed'
   img.style.left = '50%'
   img.style.top = '50%'
