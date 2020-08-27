305e7b9ee19387ace05b581bb1e6ef51e83690ff
yushirui <2514904609@qq.com>
2020-08-27 21:24:15 +0800

    feat:项目部署

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bookkeeping-website/'
+    : '/',
   lintOnSave: false,
 
   chainWebpack: config => {
